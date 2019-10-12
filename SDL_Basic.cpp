934631a1a03e34681ca3efeb43cb0f84d8c01a9b
Dafei Ning <ningdafei@gmail.com>
2019-10-11 18:59:09 -0700

    event running

--- SDL_Basic.cpp
+++ SDL_Basic.cpp
@@ -4,26 +4,40 @@
 using namespace std;
 
 int main() {
-    
+
     const int SCREEN_WIDTH = 1024;
     const int SCREEN_HEIGHT = 800;
-    
+
+    // Protection against failure of the lib.
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         cout << "SDL init failed." << endl;
         return 1;
     }
-    
+
     SDL_Window *window = SDL_CreateWindow("Partical Fire Explosion",
-                                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN
-                                          );
-    
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN
+            );
+
     if (window == NULL) {
         SDL_Quit();
         return 2;
     }
-    
-    
+
+    bool event_running = true;
+
+    SDL_Event event;
+
+    // create particle, update, check.
+    while(event_running) {
+        // Fetch the event to check if should quit.
+        while(SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                event_running = false;
+            }
+        }
+    }
+
     SDL_DestroyWindow(window);
     SDL_Quit();
 }

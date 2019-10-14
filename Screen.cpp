8945bc2ad408518e87fa511dff0ef32a6529908c
Dafei Ning <ningdafei@gmail.com>
2019-10-13 17:53:19 -0700

    split screen class

---
+++ Screen.cpp
@@ -0,0 +1,17 @@
+
+#include "Screen.h"
+
+namespace ParticleFire {
+    Screen::Screen() {
+
+    }
+    bool Screen::init() {
+
+    }
+    bool Screen::processEvent() {
+
+    }
+    void Screen::finish() {
+
+    }
+}
---
+++ Screen.h
@@ -0,0 +1,29 @@
+
+#ifndef SDL_BASIC_SCREEN_H
+#define SDL_BASIC_SCREEN_H
+
+#include <SDL.h>
+
+namespace ParticleFire {
+    class Screen {
+    public:
+        const static int SCREEN_WIDTH = 1024;
+        const static int SCREEN_HEIGHT = 800;
+    private:
+        // Conventionally make variable noted as member
+        SDL_Window *m_window;
+        SDL_Renderer *m_renderer;
+        SDL_Texture *m_texture;
+        Uint32 *m_buffer;
+
+    public:
+        Screen();
+        bool init();
+        bool processEvent();
+        void finish();
+    };
+}
+
+
+
+#endif //SDL_BASIC_SCREEN_H

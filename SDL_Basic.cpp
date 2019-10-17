c3864e647c04341704e55cdb8c60aa26b9e84c83
Dafei Ning <ningdafei@gmail.com>
2019-10-16 18:18:12 -0700

    color change by time elapsed

--- SDL_Basic.cpp
+++ SDL_Basic.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <math.h>
 #include <SDL.h>
 
 #include "Screen.h"
@@ -17,13 +18,16 @@ int main() {
 
     // create particle, update, check.
     while(true) {
-
+        // counting the seconds it takes to drive the change through sin().
+        // The sin(time_elapsed) ranges from -1 to 1.
+        int time_elapsed = SDL_GetTicks();
+        // The color index ranges from 0 to 255, make the color reside in this range.
+        double color_change = (1 + sin(time_elapsed * 0.01)) * 128;
         for (int j = 0; j < Screen::SCREEN_HEIGHT; j++) {
             for (int i = 0; i < Screen::SCREEN_WIDTH; i++) {
-                screen.setPixel(i, j, 128, 0, 255);
+                screen.setPixel(i, j, color_change, 0, 0);
             }
         }
-        //screen.setPixel(512, 400, 255, 255, 255);
         screen.update();
         if(screen.processEvent() == false) {
             break;

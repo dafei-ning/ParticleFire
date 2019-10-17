7bef6b0b4c7d7a9354f72fee15b96a9e5959dbda
Dafei Ning <ningdafei@gmail.com>
2019-10-17 15:44:05 -0700

    update setPixel

--- Screen.cpp
+++ Screen.cpp
@@ -49,6 +49,10 @@ namespace ParticleFire {
     }
 
     void Screen::setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue) {
+        // Protection through particle size
+        if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) {
+            return;
+        }
         Uint32 color = 0;
         color += red;   // 0000ff12 Red
         color <<= 8;
